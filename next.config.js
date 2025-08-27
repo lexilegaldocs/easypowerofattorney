594a7f770bac30ed06f77b1bc92421524c93180b
lexilegaldocs <lexigocom@gmail.com>
2025-08-27 15:25:00 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Avoid failing the build on lint/TS issues while launching
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-
-  experimental: { serverActions: { bodySizeLimit: '2mb' } },
 };
 module.exports = nextConfig;
